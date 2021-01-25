e17ea3a538fec2d9cc20e34a535e551dc960a95c
dmutsvanemoto <newkuda@gmail.com>
2021-01-25 08:42:55 +0000

    use material-ui to list results

--- client/src/components/List/List.tsx
+++ client/src/components/List/List.tsx
@@ -1,10 +1,23 @@
 import {ListPropType} from './ListPropType';
 import ListItem from '../ListItem';
 
+import { makeStyles } from '@material-ui/core/styles';
+import MaterialList from '@material-ui/core/List';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
+
 export const List: React.FC<ListPropType> = ({ items }) => {
-    return (<div data-testid="List">
+    const classes = useStyles();
+    
+    return (<MaterialList data-testid="List" className={classes.root}>
         {items.map(item => <ListItem key={item.geonameid} {...item}/>)}
-    </div>);
+    </MaterialList>);
 }
 
 export default List;
